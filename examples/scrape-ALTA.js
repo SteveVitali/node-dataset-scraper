98d11591f99755a7dc4e730dc7fc7739a77f2fd4
Steven Vitali <14stevevitali@gmail.com>
2020-03-02 19:09:40 -0500

    Simplify example code

--- examples/scrape-ALTA.js
+++ examples/scrape-ALTA.js
@@ -1,5 +1,4 @@
 const cheerio = require('cheerio');
-
 const Scraper = require('../lib/scraper.js');
 
 let params = {
@@ -12,20 +11,7 @@ let params = {
     failurePath: './examples/failures-map.json',
     inputPath: './examples/input.json',
     makeUrl: id =>  `https://www.alta.org/membership/directory-profile.cfm?id=${id}`,
-    defaultHeaders: {
-      'Host': 'www.alta.org',
-      'Connection': 'keep-alive',
-      'Cache-Control': 'max-age=0',
-      'Upgrade-Insecure-Requests': '1',
-      'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.108 Safari/537.36',
-      'Sec-Fetch-User': '?1',
-      'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3',
-      'Sec-Fetch-Site': 'none',
-      'Sec-Fetch-Mode': 'navigate',
-      'Referer': 'https://www.alta.org/membership/directory.cfm',
-      'Accept-Encoding': 'gzip, deflate, br',
-      'Accept-Language': 'en-US,en;q=0.9',
-    },
+    defaultHeaders: { /* ... */ },
     cookieString: `CFID=22399399; JSESSIONID=D0B2BFDB554AB4AD2E9A92A1D58F20C5.cfusion; CFTOKEN=af4855e947cb034d%2D27E76629%2DC29A%2D431A%2D8BC111A91F3A524B; _ga=GA1.2.406868278.1583191898; _gid=GA1.2.2098575155.1583191898; _gat_gtag_UA_1316255_8=1; __atuvc=4%7C10; __atuvs=5e5d9759a64070c7003`,
     extractJsonFromHtml: (alta_id, html) => {
       console.log(html, 'extracting metadata from', alta_id);
@@ -52,8 +38,8 @@ let params = {
         founded_in: parse(memberHtml, '>Founded In:</b>'),
       };
     },
-    onScrapeErr: () => { console.log('ERR'); process.exit(); },
-    onDone: () => { console.log('scraper.onDone'); }
+    onScrapeErr: () => { /* ... */ },
+    onDone: () => { /* ... */ }
 };
 
 (new Scraper(params)).run();
