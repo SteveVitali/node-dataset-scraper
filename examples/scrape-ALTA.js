b2ff9684598646748a9d083ecf2a1d0fec61f635
Steven Vitali <14stevevitali@gmail.com>
2020-03-02 19:21:05 -0500

    Clean up Scraper class

--- examples/scrape-ALTA.js
+++ examples/scrape-ALTA.js
@@ -2,22 +2,22 @@ const cheerio = require('cheerio');
 const Scraper = require('../lib/scraper.js');
 
 let params = {
-    threads: 1,
+    threads: 4,
     logEvery: 1,
     writeEvery: 1,
     inputIndex: 0,
+    inputPath: './examples/input.json',
     outputPath: './examples/output-map.json',
     logsPath: './examples/logs.log',
     failurePath: './examples/failures-map.json',
-    inputPath: './examples/input.json',
     makeUrl: id =>  `https://www.alta.org/membership/directory-profile.cfm?id=${id}`,
-    defaultHeaders: { /* ... */ },
+    defaultHttpHeaders: { /* ... */ },
     cookieString: `CFID=22399399; JSESSIONID=D0B2BFDB554AB4AD2E9A92A1D58F20C5.cfusion; CFTOKEN=af4855e947cb034d%2D27E76629%2DC29A%2D431A%2D8BC111A91F3A524B; _ga=GA1.2.406868278.1583191898; _gid=GA1.2.2098575155.1583191898; _gat_gtag_UA_1316255_8=1; __atuvc=4%7C10; __atuvs=5e5d9759a64070c7003`,
-    extractJsonFromHtml: (alta_id, html) => {
-      console.log(html, 'extracting metadata from', alta_id);
+    extractJsonFromHtml: (id, html) => {
+      console.log(html, 'extracting metadata from', id);
       if (html.indexOf('Search for Members') >= 0 ||
           html.indexOf('Sorry, no results.') >= 0) {
-        return { id_checksum: alta_id, is_null: true };
+        return { id_checksum: id, is_null: true };
       }
       const $ = cheerio.load(html);
       const memberHtml = $('.member-profile').html();
